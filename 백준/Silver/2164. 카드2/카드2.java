ccf3b6af6098cc58f6b14575e7cc93d8995d175d
choyunju <99892677+choyunju@users.noreply.github.com>
2024-06-10 23:36:56 +0900

    [Silver IV] Title: 카드2, Time: 184 ms, Memory: 45572 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -0,0 +1,21 @@
+//no. 2164
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(reader.readLine());
+		Queue<Integer> queue = new LinkedList<>();
+		
+		for(int i=1; i<=N; i++) {
+			queue.add(i);
+		}
+		while(queue.size()>1) {
+			queue.poll();
+			queue.add(queue.poll());
+		}
+		
+		System.out.println(queue.poll());
+	}
+}
