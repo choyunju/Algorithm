dbf07eee8502e0d2c41c9f74b5bdd1fc620d2f2b
choyunju <99892677+choyunju@users.noreply.github.com>
2024-06-22 15:11:31 +0900

    [Silver I] Title: 기타 레슨, Time: 312 ms, Memory: 24080 KB -BaekjoonHub

--- 백준/Silver/2343. 기타 레슨/기타 레슨.java
+++ 백준/Silver/2343. 기타 레슨/기타 레슨.java
@@ -1,31 +1,24 @@
-//문제30 - 2343
+//no. 2343
 import java.util.*;
 import java.io.*;
 
-public class Main {
+class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer token = new StringTokenizer(reader.readLine());
 		int N = Integer.parseInt(token.nextToken());
 		int M = Integer.parseInt(token.nextToken());
-		
 		int[] n = new int[N];
 		token = new StringTokenizer(reader.readLine());
-		
-		int start = 0;
-		int end = 0;
+		int low=0, high = 0;
 		for(int i=0; i<N; i++) {
 			n[i] = Integer.parseInt(token.nextToken());
-			if(start < n[i]) {
-				start = n[i];
-			}
-			end += n[i];
-		
+			low = Math.max(low, n[i]);
+			high += n[i];
 		}
-		
-		
-		while(start <= end) {
-			int mid = (start + end) / 2;
+	
+		while(low <= high) {
+			int mid = (low+high)/2;
 			int sum = 0;
 			int count = 0;
 			for(int i=0; i<N; i++) {
@@ -38,13 +31,13 @@ public class Main {
 			if(sum != 0) {
 				count++;
 			}
-			if(count <= M) {
-				end = mid - 1;
+			if(count<=M) {
+				high = mid-1;
 			}
 			else {
-				start = mid + 1;
+				low = mid+1;
 			}
 		}
-		System.out.println(start);
+		System.out.println(low);
 	}
 }
