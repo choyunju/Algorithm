9bf3c971946f3ce61051a79853ed16c52413c5f4
choyunju <99892677+choyunju@users.noreply.github.com>
2024-10-03 12:42:20 +0900

    [Silver II] Title: 스택 수열, Time: 320 ms, Memory: 28692 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,45 +1,63 @@
+//no.9 - 1874
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		int n = Integer.parseInt(reader.readLine());
-		int numArray[] = new int[n];
-		Stack<Integer> stack = new Stack<Integer>();
-		
-		for(int i=0; i<n; i++) {
-			numArray[i] = Integer.parseInt(reader.readLine());
+		StringBuilder builder = new StringBuilder();
+		int N = Integer.parseInt(reader.readLine());
+		Stack<Integer> stack = new Stack<>();
+		int[] arr = new int[N];
+		for(int i=0; i<N; i++) {
+			arr[i] = Integer.parseInt(reader.readLine());
 		}
-		StringBuilder result = new StringBuilder();
 		
-		int number = 1;
-		boolean b = true;
-		for(int i=0; i<n; i++) {
-			if(numArray[i] >= number) {
-				while(numArray[i] >= number) {
-					stack.push(number);
-					number++;
-					result.append("+\n");
-				}
+		
+//		while(num <=N) {
+//			int a = arr[index];
+//			if(stack.isEmpty()) {
+//				stack.add(num);
+//				num++;
+//				builder.append("+\n");
+//			}
+//			if(stack.peek() == a) {
+//				stack.pop();
+//				index++;
+//				builder.append("-\n");
+//			} else if(stack.peek() < a) {
+//				stack.add(num);
+//				num++;
+//				builder.append("+\n");
+//			} else if(stack.peek() > a) {
+//				builder = new StringBuilder();
+//				builder.append("NO");
+//				break;
+//			}
+//		}
+		
+		int num = 1;
+		for(int i=0; i<N; i++) {
+			int a = arr[i];
+			if(stack.isEmpty() && num<=N) {
+				stack.add(num);
+				num++;
+				builder.append("+\n");
+			}
+			while(stack.peek() < a && num <=N) {
+				stack.add(num);
+				num++;
+				builder.append("+\n");
+			}
+			if(stack.peek() == a) {
 				stack.pop();
-				result.append("-\n");
-			}
-			else {
-				int peek = stack.pop();
-				if(peek > numArray[i]) {
-					System.out.print("NO");
-					b = false;
-					break;
-				}
-				else {
-					result.append("-\n");
-				}
+				builder.append("-\n");
+			} else if(stack.peek() > a) {
+				builder = new StringBuilder();
+				builder.append("NO");
+				break;
 			}
 		}
-		if(b) {
-			System.out.print(result);
-		}
-		
+		System.out.println(builder);
 	}
 }
