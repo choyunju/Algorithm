d664bcd55be7e5aef19a06be73c86c46b3369a04
choyunju <99892677+choyunju@users.noreply.github.com>
2025-02-15 14:50:03 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16468 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,34 +1,29 @@
-//no.10 - 10799
 import java.io.*;
 import java.util.*;
 
-class Main {
+public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		Stack<String> stack = new Stack<>();
-		String[] s = reader.readLine().split("");
-		int count = 0;
-		int result = 0;
-
+		char[] s = reader.readLine().toCharArray();
+		
+		int total = 0;  //잘려진 조각의 총 개수
+		
+		Stack<Character> stack = new Stack<>();
+		
 		for(int i=0; i<s.length-1; i++) {
-			String g = s[i];
-			int post = i-1;
-			int next = i+1;
-			if(g.equals("(")) {
-				if(!s[next].equals(")")) {
-					count++;
-					result++;
+			if(s[i] == '(') {
+				if(s[i+1] == '(') {
+					total++;
+
 				}
-				stack.add(g);
-			} else if(g.equals(")")) {
-				if(s[post].equals(")")) {
-					count--;
-				} else if(s[post].equals("(")) {
-					result = result + count;
-				}
+				stack.add(s[i]);
+			} else {
 				stack.pop();
+				if(s[i-1] == '(') {
+					total += stack.size();
+				}
 			}
 		}
-		System.out.println(result);
+		System.out.println(total);
 	}
 }
