096cc8bef88108f806139491accd4beb4cdb442d
choyunju <99892677+choyunju@users.noreply.github.com>
2025-02-25 17:19:58 +0900

    [Silver I] Title: 트리 순회, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,98 +1,80 @@
-import java.util.*;
 import java.io.*;
-
-class Main {
-	static boolean[] visited;
-	static StringBuilder builder;
-	static ArrayList<Node>[] list;
-	public static void main(String[] args) throws IOException {
-		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		builder = new StringBuilder();
-		
-		int N = Integer.parseInt(reader.readLine());
-		visited = new boolean[N];
-		list = new ArrayList[N];
-		for(int i=0; i<N; i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		for(int i=0; i<N; i++) {
-			StringTokenizer token = new StringTokenizer(reader.readLine());
-			int node = (int)(token.nextToken().charAt(0)) - 65;
-			int left = (int)(token.nextToken().charAt(0)) - 65;
-			int right = (int)(token.nextToken().charAt(0)) - 65;
-			
-			list[node].add(new Node(left, right));
-		}
-		
-		preOrder(0);
-		System.out.println(builder);
-		
-		builder = new StringBuilder();
-		visited = new boolean[N];
-		inOrder(0);
-		System.out.println(builder);
-		
-		builder = new StringBuilder();
-		visited = new boolean[N];
-		postOrder(0);
-		System.out.println(builder);
-	}
-	
-	static void preOrder(int node) {
-		visited[node] = true;
-		builder.append((char)(node + 65));
-		
-		for(Node next : list[node]) {
-			int left = next.left;
-			int right = next.right;
-			if(left != -19 && !visited[left]) {
-				preOrder(left);
-			}
-			if(right != -19 && !visited[right]) {
-				preOrder(right);
-			}
-		}
-	}
-	
-	public static void inOrder(int node) {
-		visited[node] = true;
-		for(Node next : list[node]) {
-			int left = next.left;
-			int right = next.right;
-			if(left != -19 && !visited[left]) {
-				inOrder(left);
-			}
-			builder.append((char)(node+65));
-			if(right != -19 && !visited[right]) {
-				inOrder(right);
-			}
-		}
-	}
-	
-	public static void postOrder(int node) {
-		visited[node] = true;
-		for(Node next : list[node]) {
-			int left = next.left;
-			int right = next.right;
-			if(left != -19 && !visited[left]) {
-				postOrder(left);
-			}
-			if(right != -19 && !visited[right]) {
-				postOrder(right);
-			}
-			builder.append((char)(node+65));
-		}
-		
-	}
-}
+import java.util.*;
 
 class Node {
+	int num;
 	int left;
 	int right;
 	
-	public Node(int left, int right) {
+	public Node(int num, int left, int right) {
+		this.num = num;
 		this.left = left;
 		this.right = right;
+	}	
+	
+}
+
+public class Main {
+	static StringBuilder builder;
+	static Node[] tree;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		builder = new StringBuilder();
+		int N = Integer.parseInt(reader.readLine());
+		tree = new Node[N+1];
+		
+		for(int i=0; i<N; i++) {
+			char[] c = reader.readLine().toCharArray();
+			int n = (int)c[0] - 64;
+			int left = 0;
+			int right = 0;
+			if(c[2] == '.') {
+				left = 0;
+			} else {
+				left = (int)c[2] - 64;
+			}
+			if(c[4] == '.') {
+				right = 0;
+			} else {
+				right = (int)c[4] - 64;
+			}
+			tree[n] = new Node(n, left, right);
+			
+		}
+		
+		preOrder(tree[1]);
+		builder.append("\n");
+		inOrder(tree[1]);
+		builder.append("\n");
+		postOrder(tree[1]);
+		System.out.println(builder);
+	}
+	
+	public static void preOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+		builder.append((char)(node.num + 64));
+		preOrder(tree[node.left]);
+		preOrder(tree[node.right]);
+	}
+	
+	public static void inOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+		inOrder(tree[node.left]);
+		builder.append((char)(node.num+64));
+		inOrder(tree[node.right]);
+	}
+	
+	public static void postOrder(Node node) {
+		if(node == null) {
+			return;
+		}
+		postOrder(tree[node.left]);
+		postOrder(tree[node.right]);
+		builder.append((char)(node.num + 64));
 	}
 }
