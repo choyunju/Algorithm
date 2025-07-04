33448f7d038dca6a452b9755bb4ebf08cf4fa5cd
choyunju <99892677+choyunju@users.noreply.github.com>
2025-07-04 19:29:01 +0900

    [Silver I] Title: 경로 찾기, Time: 208 ms, Memory: 18632 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,37 +1,61 @@
-//문제 62 - 11403
 import java.io.*;
 import java.util.*;
 
-public class Main {
+class Main {
+	static ArrayList<Integer>[] list;
+	static int[][] map;
+	static int N;
+
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
-		int N = Integer.parseInt(reader.readLine());
-		int[][] D = new int[N+1][N+1];
-		
-		for(int i=1; i<=N; i++) {
-			StringTokenizer token = new StringTokenizer(reader.readLine());
-			for(int j=1; j<=N; j++) {
-				D[i][j] = Integer.parseInt(token.nextToken());
-			}
+		N = Integer.parseInt(reader.readLine());
+		list = new ArrayList[N];
+		map = new int[N][N];
+		for(int i=0; i<N; i++) {
+			list[i] = new ArrayList<>();
 		}
 		
-		for(int k=1; k<=N; k++) {
-			for(int s=1; s<=N; s++) {
-				for(int e=1; e<=N; e++) {
-					if(D[s][k] == 1 && D[k][e] == 1) {
-						D[s][e] = 1;
-					}	
+		for(int i=0; i<N; i++) {
+			StringTokenizer token = new StringTokenizer(reader.readLine());
+	
+			for(int j=0; j<N; j++) {
+				int node = Integer.parseInt(token.nextToken());
+				if(node != 0) {
+					list[i].add(j);
+					map[i][j] = 1;
 				}
 			}
 		}
-		
-		for(int i=1; i<=N; i++) {
-			for(int j=1; j<=N; j++) {
-				writer.write(D[i][j] + " ");
-			}
-			writer.write("\n");
+	
+		for(int i=0; i<N; i++) {
+			BFS(i);
 		}
-		writer.flush();
+		
+		StringBuilder builder = new StringBuilder();
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				builder.append(map[i][j] + " ");
+			}
+			builder.append("\n");
+		}
+		System.out.println(builder);
+	}
+	
+	public static void BFS(int node) {
+		boolean[] visited = new boolean[N];
+		Queue<Integer> queue = new LinkedList<>();
+		queue.add(node);
+		
+		while(!queue.isEmpty()) {
+			int now = queue.poll();
+			for(int next : list[now]) {
+				if(!visited[next]) {
+					visited[next] = true;
+					queue.add(next);
+					map[node][next] = 1;
+				}
+			}
+		}
 	}
 }
